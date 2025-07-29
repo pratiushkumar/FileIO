0344b3ef526217ec0393ff10b0f2d9ef04539993
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-07-29 23:44:10 +0530

    example

---
+++ src/example.java
@@ -0,0 +1,11 @@
+public class example
+{
+    public static <T>  void printELmemnt( T element ) {
+        System.out.println(element);
+    }
+    public static void main(String[] args) {
+        int x =5;
+        printELmemnt(x);
+    }
+
+}
